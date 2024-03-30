80345d206353fbd467c7d95abfa1fe8fc70737bf
ChromMob <filip.chromy04@gmail.com>
2024-03-30 21:53:44 +0100

    Remove id read from Config

--- src/main/java/me/chrommob/fakeplayerapi/FakePlayerAPI.java
+++ src/main/java/me/chrommob/fakeplayerapi/FakePlayerAPI.java
@@ -39,7 +39,7 @@ public class FakePlayerAPI {
         config = new Config("config.yml");
         ConfigManager configManager = new ConfigManager(dataDirectory.toFile());
         configManager.addConfig(config);
-        database = new Database(UUID.fromString(config.getKey("id").getAsString()), config.getKey("mysql").getKey("host").getAsString(), config.getKey("mysql").getKey("port").getAsInt(), config.getKey("mysql").getKey("database").getAsString(), config.getKey("mysql").getKey("username").getAsString(), config.getKey("mysql").getKey("password").getAsString());
+        database = new Database(null, config.getKey("mysql").getKey("host").getAsString(), config.getKey("mysql").getKey("port").getAsInt(), config.getKey("mysql").getKey("database").getAsString(), config.getKey("mysql").getKey("username").getAsString(), config.getKey("mysql").getKey("password").getAsString());
     }
 
     public static FakePlayerAPI getInstance() {
